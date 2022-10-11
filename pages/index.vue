725f3b9bc80cf1b966d8eee19d75bddc101fca57
elkapre <khefrey123@gmail.com>
2022-10-11 21:38:37 +0800

    commit login page

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,46 @@
 <template>
-<body>
-    <div>
-      <div class="mx-auto mt-4" style="width:200px">
-         <img class="rounded-sm " src="Picture1.png">       
-      </div>        
-    <div class="mx-auto border border-dark rounded mt-4" style="width:500px" >    
+<body class="bg-white">
+  <div>
+    <div class="mx-auto mt-4" style="width:200px">
+      <img class="rounded-sm " src="Picture1.png">       
+    </div>        
+    <div class="mx-auto border border-dark rounded mt-4 bg-warning" style="width:500px" >    
       <forms >
         <div class="form-group m-3" >
-          <label class="align-content-center">Email Address</label>
-          <input  id="email" type="email" class="form-control align-content-center" placeholder="Please Input Email" >
+          <label class="align-content-center font">Email Address</label>
+          <input  id="email" type="email" class="form-control align-content-center"  placeholder="Please Input Email" >
         </div>
         <div class="m-3">
           <label class="align-content-center">Password</label>
-          <input id="password"  type="password"  class="form-control align-content-center" placeholder="Please input Password">
-          </div>
-        <div class="mx-auto" style="width:100px">
-          <button type="submit" class="btn btn-primary my-3">Submit</button>
-        </div>  
+          <input id="password"  type="password"  class="form-control align-content-center"  placeholder="Please input Password">
+        </div>
+        <div class="container">
+          <div class="row g-2 mb-3">
+            <div class="col-6">
+              <div class="form-check">
+                <input class="form-check-input ml=2" type="checkbox">
+                <label class="check-label">Remember me</label>
+              </div>
+            </div>
+            <div class="col-6 ">             
+              <a href="#" class="" style="float:right" >Forgot Password</a> 
+            </div>
+          </div>        
+        </div>
+        <div class="container text-left">
+          <div class="row g-2 mb-3">
+            <div class="col-6 fs-6" style="width:50px">
+              Don't have account?
+              <a href="#" class=" ">Register Here</a> 
+            </div>
+            <div class="col-6" >
+            <button type="submit" class="btn btn-primary bg-danger border-warning form-control" >Submit</button>
+            </div>  
+          </div>          
+        </div>        
       </forms>
     </div>
-    </div>
+  </div>
   </body>
 </template>
 
